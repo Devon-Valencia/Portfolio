36b3692fcad971890f81ce214a7b5af1eb84eb11
devon-valencia <devonval06@gmail.com>
2025-08-13 17:40:10 -0600

    progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,20 @@ export default function Home() {
       <div id="stars3"></div>
 
       <main>
-        <header className="Header">
-          <nav className="info">
-            <a href="">Home</a> <a href="#about">About</a> <a href="#contact">Contact</a>
-          </nav>
-        </header>
+        <div className='container'>
+          <header className="Header">
+            <nav className="info">
+              <a href="" className='home'>Home</a> <a href="#about">Skills</a> <a href ="#projects">Projects</a> <a href="#contact">Contact</a>
+            </nav>
+          </header>
 
-        <h1 className="aboutmebox">
-          <img
-            className="profile-pic"
-            src="Devon Valencia.png"
-            alt="Profile Picture"
-          />
-          <span className="aboutme-text">Devon Valencia</span>
-        </h1>
+          <h1 className="aboutmebox">
+            <span className="aboutme-text">Hi, I'm 
+              <span className='highlight'> Devon Valencia</span>
+              <span className='aboutme-text2'>Full stack Software Engineer</span>
+            </span>
+          </h1>
+        </div>
       </main>
     </>
   );
